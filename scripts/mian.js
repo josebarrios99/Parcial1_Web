ccd48e516e24821ccdd8cbcf56f57dc927d58c1d
Jose <estaravia@hotmail.com>
2020-03-13 09:22:47 -0500

    Initial commit

---
+++ scripts/mian.js
@@ -0,0 +1,10 @@
+var changeImage = document.querySelector('.changeImage__image');
+var ChangeInput = document.querySelector('.changeImage__input');
+var lin1 = 'https://source.unsplash.com/100x100'
+var link2 = 'https://source.unsplash.com/320x320';
+function Change () {
+    
+    
+}
+Change();
+ChangeInput.addEventListener('input', Change);
